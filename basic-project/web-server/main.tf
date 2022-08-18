3cacb892098f148015b6e439e0be79c102b15df4
kattel49 <shubhushankattel@gmail.com>
2022-08-18 11:19:17 +0200

    Reconfigured the directory structure, and created ansible files

--- basic-project/web-server/main.tf
+++ basic-project/web-server/main.tf
@@ -10,29 +10,51 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-variable "instance_name" {
+variable "web_server_name" {
   default = "web-server"
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name = "allow-ssh"
-  ingress {
-    cidr_blocks = [ "0.0.0.0/0" ]
-    description = "allow ssh"
-    from_port = 22
-    protocol = "tcp"
-    self = false
-    to_port = 22
-  }
+variable "jenkins_server_name" {
+  default = "jenkins-server"
 }
-
+#empty security group
+resource "aws_security_group" "allow_ssh" {
+  name = "allow_ssh"
+}
+#ingress rule attached to the security group to allow ssh
+resource "aws_security_group_rule" "allow_ssh" {
+  type = "ingress"
+  from_port = 22
+  to_port = 22
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.allow_ssh.id}"
+  protocol = "tcp"
+}
+#web-server instance
 resource "aws_instance" "web-server" {
     instance_type = "t2.micro"
     key_name = "projects"
     ami = "ami-065deacbcaac64cf2"
     tags = {
-      "Name" = "${var.instance_name}"
+      "Name" = "${var.web_server_name}"
     }
     vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
 }
-
+#jenkins-server instance
+resource "aws_instance" "jenkins-server" {
+  ami = "ami-065deacbcaac64cf2"
+  instance_type = "t2.micro"
+  key_name = "projects"
+  tags = {
+    Name = "${var.jenkins_server_name}"
+  }
+  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
+}
+#ip address of the web server
+output "web_ip" {
+  value = "${aws_instance.web-server.public_ip}"
+}
+#ip address of the jenkins server
+output "jenkins_ip"{
+  value = "${aws_instance.jenkins-server.public_ip}"
+}
